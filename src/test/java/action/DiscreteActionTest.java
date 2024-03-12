64b061255ff20e68ee8ff5e39e3680647854c0ee
Trikzy7 <theoplebani2002@icloud.com>
2024-03-12 14:33:52 +0100

    [SETUP] Set Up Jenkins

---
+++ src/test/java/action/DiscreteActionTest.java
@@ -0,0 +1,46 @@
+package action;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscreteActionTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spendTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMethod() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentLapsTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getObject() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void next() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasNext() {
+    }
+}
