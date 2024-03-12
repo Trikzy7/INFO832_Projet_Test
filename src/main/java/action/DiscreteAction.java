e083b5f4d27839d728ba2b3cbe1035c953903036
Trikzy7 <theoplebani2002@icloud.com>
2024-03-12 14:44:07 +0100

    [TEST] test launch build project jenkins

--- src/main/java/action/DiscreteAction.java
+++ src/main/java/action/DiscreteAction.java
@@ -23,6 +23,7 @@ public class DiscreteAction implements DiscreteActionInterface {
 	private Method method;
 	
 //	prout
+//	prout
 	private Timer timmer;				// timer provides new lapsTime
 	//private TreeSet<Integer> dates;	// obsolete, managed in timer 
 	//private Vector<Integer> lapsTimes;// obsolete, managed in timer
