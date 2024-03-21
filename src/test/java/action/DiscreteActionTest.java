2f9bd397c22f45bb3f6b6f5efe04f5e76d949c8c
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-03-21 12:25:34 +0100

    [FEATURE] : tests A3

--- src/test/java/action/DiscreteActionTest.java
+++ src/test/java/action/DiscreteActionTest.java
@@ -2,6 +2,7 @@ package action;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Before;
 import org.junit.Test;
 import timer.Timer;
@@ -56,6 +57,29 @@ public class DiscreteActionTest {
         // Then
         assertNotNull("LapsTime should not be null after spendTime", action.getCurrentLapsTime());
         assertEquals("LapsTime should be decreased by the time spent", expectedLapsTime, action.getCurrentLapsTime());
+    }
+    @Test
+    public void testNextMethodUpdatesLapsTime() {
+
+        //action = new DiscreteAction(testObject, methodName, mockTimer);
+
+        // Set the initial lapsTime
+        //action.setCurrentLapsTime(initialLapsTime); // there is no method to set LapsTime
+
+        // Capture the old lapsTime before calling next()
+        Integer oldLapsTime = action.getCurrentLapsTime();
+
+        // When
+        action.next(); // Calls Timer.next() internally, which is mocked
+
+        // Then
+        Integer newLapsTime = action.getCurrentLapsTime();
+        assertNotNull("LapsTime should not be null after next()", newLapsTime);
+        assertNotEquals("LapsTime should be updated (not equal to the old value) after next()",
+                oldLapsTime, newLapsTime);
+        // Optionally, check if the newLapsTime matches what the mock Timer returns
+        /*assertEquals("LapsTime should be updated to the new value provided by the Timer",
+                mockTimer.next(), newLapsTime);*/ //Waiting for mock in pom.xml
     }
 
 }
