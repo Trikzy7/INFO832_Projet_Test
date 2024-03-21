b75a657c0ddf1a4b9964547fb9f7269a9c2ac70d
lilcdx <lila.cadoux@gmail.com>
2024-03-21 09:00:52 +0100

    [ADD]Set up timer test folder

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+\
+,src/test/java/action/DiscreteActionTest.java,b\e\be0864ea93eb462c624962d46790ae41105a49e0
+R
+"src/main/java/timer/DateTimer.java,7\8\78be95389f16615ec1ee42431aecd2d7aa51435b
+V
+&src/test/java/timer/DateTimerTest.java,a\5\a5427e42f819a1452b94a46ae24616eeadea9bd4
---
+++ src/test/java/timer/DateTimerTest.java
@@ -0,0 +1,26 @@
+package timer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTimerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void hasNext() {
+    }
+
+    @Test
+    void next() {
+    }
+}
