8c6557778c18e79e206d1508ce7d2a2c061c315e
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-03-21 12:18:48 +0100

    [FEATURE] : tests A1 et A2

--- src/test/java/action/DiscreteActionTest.java
+++ src/test/java/action/DiscreteActionTest.java
@@ -1,46 +1,61 @@
 package action;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import timer.Timer;
+import java.lang.reflect.Method;
 
-class DiscreteActionTest {
+public class DiscreteActionTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    private DiscreteAction action;
+    private Timer timer;
+    private Object testObject;
+    private String methodName = "toString";
+    private Integer initialLapsTime = 10; // Initial lapsTime to be set for testing
+
+    @Before
+    public void setUp() throws NoSuchMethodException, SecurityException {
+        // Initialize necessary objects
+        testObject = new Object();
+        //timer = new Timer(); // Timer is an interface
+
+        // Assume setCurrentLapsTime is a method in DiscreteAction to set the lapsTime for testing
+        action = new DiscreteAction(testObject, methodName, timer);
+        //action.setCurrentLapsTime(initialLapsTime); // This method must be added to your DiscreteAction class
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @Test
+    public void testDiscreteActionConstructor() {
+        // Vérification que l'objet est correctement initialisé
+        assertEquals("L'objet passé au constructeur doit être le même que l'objet retourné par getObject()", testObject, action.getObject());
+
+        // Vérification que la méthode est correctement initialisée
+        Method expectedMethod = null;
+        try {
+            expectedMethod = testObject.getClass().getMethod(methodName);
+        } catch (NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+        }
+        assertEquals("La méthode passée au constructeur doit être la même que celle retournée par getMethod()", expectedMethod, action.getMethod());
+
+        // Vérification que le Timer est correctement initialisé
+        // assertEquals("Le timer passé au constructeur doit être le même que celui retourné par getTimmer()", timer, action.getTimmer()); // Uncomment when getTimmer method is available
     }
 
-    @org.junit.jupiter.api.Test
-    void spendTime() {
+    @Test
+    public void testSpendTime() {
+        // Given
+        int timeToSpend = 5;
+        Integer expectedLapsTime = initialLapsTime - timeToSpend;
+
+        // When
+        action.spendTime(timeToSpend);
+
+        // Then
+        assertNotNull("LapsTime should not be null after spendTime", action.getCurrentLapsTime());
+        assertEquals("LapsTime should be decreased by the time spent", expectedLapsTime, action.getCurrentLapsTime());
     }
 
-    @org.junit.jupiter.api.Test
-    void getMethod() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getCurrentLapsTime() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getObject() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void compareTo() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testToString() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void next() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void hasNext() {
-    }
 }
