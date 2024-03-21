197e774d5c18488877aac69db98c0c241bc952fb
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-03-21 09:14:15 +0100

    dossier discreteBehaviorSimulator

---
+++ src/test/java/discreteBehaviorSimulator/ClockTest.java
@@ -0,0 +1,60 @@
+package discreteBehaviorSimulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClockTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void addObserver() {
+    }
+
+    @Test
+    void removeObserver() {
+    }
+
+    @Test
+    void setVirtual() {
+    }
+
+    @Test
+    void isVirtual() {
+    }
+
+    @Test
+    void setNextJump() {
+    }
+
+    @Test
+    void increase() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void lockReadAccess() {
+    }
+
+    @Test
+    void unlockReadAccess() {
+    }
+
+    @Test
+    void lockWriteAccess() {
+    }
+
+    @Test
+    void unlockWriteAccess() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
