645f436996c2c0284563b676920aec1387c01faa
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-05-16 14:47:46 +0200

    [UPDATE] : JavaDoc pour TimeBoundedTimer.java et OneShotTimer.java

--- src/main/java/timer/OneShotTimer.java
+++ src/main/java/timer/OneShotTimer.java
@@ -1,25 +1,49 @@
 package timer;
 
+/**
+ * Represents a one-shot timer that fires only once.
+ * The timer can be queried for the presence of a next value and retrieved if present.
+ */
 public class OneShotTimer implements Timer {
 
-	private Integer at;
-	private boolean hasNext;
+	private Integer at; // The value of the timer
+	private boolean hasNext; // Flag indicating whether the timer has a next value
 
+	/**
+	 * Constructs a OneShotTimer with the specified value.
+	 * If the value is 1, hasNext will be false, indicating that there is no next value.
+	 *
+	 * @param at The value of the timer.
+	 */
 	public OneShotTimer(int at) {
 		this.at = at;
-		this.hasNext = at !=1; // If at is not 1, hasNext will be true
+		this.hasNext = at != 1; // If at is not 1, hasNext will be true
 	}
 
+	/**
+	 * Checks whether the timer has a next value.
+	 *
+	 * @return true if the timer has a next value, false otherwise.
+	 */
 	@Override
 	public boolean hasNext() {
 		return this.hasNext;
 	}
 
+	/**
+	 * Retrieves the next value of the timer.
+	 * If hasNext is true, returns the value without marking it as consumed.
+	 * If hasNext is false, returns null.
+	 *
+	 * @return The next value of the timer, or null if there is no next value.
+	 */
 	@Override
 	public Integer next() {
-		if (this.hasNext) {  // Ensures that next is still considering the timer active
-			return this.at; // Returns the value but does not mark it as consumed
-		}
-		return null; // If hasNext is false, return null
-	}
+        Integer result = null;
+        if (this.hasNext) { // Ensures that next is still considering the timer active
+            result = this.at;// Returns the value but does not mark it as consumed
+        } // If hasNext is false, return null
+
+        return result;
+    }
 }
--- src/main/java/timer/TimeBoundedTimer.java
+++ src/main/java/timer/TimeBoundedTimer.java
@@ -1,15 +1,26 @@
 package timer;
 
+/**
+ * Represents a timer bounded by a specified time interval.
+ * This timer wraps around another timer and only provides values within the specified time interval.
+ */
 public class TimeBoundedTimer implements Timer {
-	
-	private Timer timer2bound;
-	private Integer startTime;
-	private Integer stopTime;
-	
-	private Integer next=0;
-	private int time=0;
-	private boolean hasNext;
 
+	private Timer timer2bound; // The timer to be bounded
+	private Integer startTime; // The start time of the interval
+	private Integer stopTime; // The stop time of the interval
+
+	private Integer next = 0; // The next value of the timer
+	private int time = 0; // The accumulated time
+	private boolean hasNext; // Flag indicating whether the timer has a next value
+
+	/**
+	 * Constructs a TimeBoundedTimer with the specified parameters.
+	 *
+	 * @param timer2bound The timer to be bounded.
+	 * @param startTime   The start time of the interval.
+	 * @param stopTime    The stop time of the interval.
+	 */
 	public TimeBoundedTimer(Timer timer2bound, int startTime, int stopTime) {
 		this.timer2bound = timer2bound;
 		this.startTime = startTime;
@@ -17,42 +28,61 @@ public class TimeBoundedTimer implements Timer {
 		this.init();
 	}
 
+	/**
+	 * Constructs a TimeBoundedTimer with the specified parameters, setting stopTime to Integer.MAX_VALUE.
+	 *
+	 * @param timer2bound The timer to be bounded.
+	 * @param startTime   The start time of the interval.
+	 */
 	public TimeBoundedTimer(Timer timer2bound, int startTime) {
 		this.timer2bound = timer2bound;
 		this.startTime = startTime;
 		this.stopTime = Integer.MAX_VALUE;
 		this.init();
 	}
-	
+
+	/**
+	 * Initializes the timer by advancing it to the next value within the time interval.
+	 */
 	private void init() {
 		this.next = this.timer2bound.next();
 		while (this.next < this.startTime) {
 			this.next += this.timer2bound.next();
 		}
-		if(this.next<this.stopTime) {
+		if (this.next < this.stopTime) {
 			this.hasNext = true;
-		}else {
+		} else {
 			this.hasNext = false;
 		}
 	}
 
+	/**
+	 * Checks whether the timer has a next value within the time interval.
+	 *
+	 * @return true if the timer has a next value within the time interval, false otherwise.
+	 */
 	@Override
 	public boolean hasNext() {
-		return this.hasNext ;
+		return this.hasNext;
 	}
 
+	/**
+	 * Retrieves the next value of the timer within the time interval.
+	 * If the accumulated time exceeds the stop time, returns null.
+	 *
+	 * @return The next value of the timer within the time interval, or null if the stop time is exceeded.
+	 */
 	@Override
 	public Integer next() {
-		Integer next = this.next;
-		this.time+=this.next;
-		if(this.time < this.stopTime) {
+		Integer nextValue = this.next;
+		this.time += this.next;
+		if (this.time < this.stopTime) {
 			this.next = this.timer2bound.next();
-			this.hasNext=false;
-		}else {
-			next = null;
-			this.hasNext=false;
+			this.hasNext = false;
+		} else {
+			nextValue = null;
+			this.hasNext = false;
 		}
-		return next;
+		return nextValue;
 	}
-
 }
